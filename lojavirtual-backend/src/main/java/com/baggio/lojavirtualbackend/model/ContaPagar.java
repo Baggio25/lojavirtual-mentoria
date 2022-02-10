cd7cae9ee2d1641be048bdcbf1f98d03129abbf4
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-02-10 08:57:03 -0300

    Criado Models: ContaReceber, ContaPagar, FormaPagamento e Enum StatusContaPagar

--- lojavirtual-backend/src/main/java/com/baggio/lojavirtualbackend/model/ContaPagar.java
+++ lojavirtual-backend/src/main/java/com/baggio/lojavirtualbackend/model/ContaPagar.java
@@ -21,17 +21,17 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import com.baggio.lojavirtualbackend.model.enums.StatusContaReceber;
+import com.baggio.lojavirtualbackend.model.enums.StatusContaPagar;
 
 @Entity
-@Table(name = "conta_receber")
-@SequenceGenerator(name = "seq_conta_receber", sequenceName = "seq_conta_receber", allocationSize = 1, initialValue = 1)
-public class ContaReceber implements Serializable {
+@Table(name = "conta_pagar")
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+public class ContaPagar implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_receber")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
 	private Long id;
 
 	@Column(nullable = false)
@@ -39,7 +39,7 @@ public class ContaReceber implements Serializable {
 
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
-	private StatusContaReceber status;
+	private StatusContaPagar status;
 
 	@Temporal(TemporalType.DATE)
 	@Column(nullable = false, name = "data_vencimento")
@@ -59,15 +59,19 @@ public class ContaReceber implements Serializable {
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
 
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_fornecedor_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fornecedor_fk"))
+	private Pessoa pessoaFornecedor;
+	
 	@ManyToOne(targetEntity = FormaPagamento.class)
 	@JoinColumn(name = "pagamento_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pagamento_fk"))
 	private FormaPagamento formaPagamento;
 
-	public ContaReceber() {
+	public ContaPagar() {
 	}
 
-	public ContaReceber(Long id, String descricao, StatusContaReceber status, Date dataVencimento, Date dataPagamento,
-			BigDecimal valorTotal, BigDecimal valorDesconto, Pessoa pessoa, FormaPagamento formaPagamento) {
+	public ContaPagar(Long id, String descricao, StatusContaPagar status, Date dataVencimento, Date dataPagamento,
+			BigDecimal valorTotal, BigDecimal valorDesconto, Pessoa pessoa, Pessoa pessoaFornecedor, FormaPagamento formaPagamento) {
 		this.id = id;
 		this.descricao = descricao;
 		this.status = status;
@@ -76,6 +80,7 @@ public class ContaReceber implements Serializable {
 		this.valorTotal = valorTotal;
 		this.valorDesconto = valorDesconto;
 		this.pessoa = pessoa;
+		this.pessoaFornecedor = pessoaFornecedor;
 		this.formaPagamento = formaPagamento;
 	}
 
@@ -95,11 +100,11 @@ public class ContaReceber implements Serializable {
 		this.descricao = descricao;
 	}
 
-	public StatusContaReceber getStatus() {
+	public StatusContaPagar getStatus() {
 		return status;
 	}
 
-	public void setStatus(StatusContaReceber status) {
+	public void setStatus(StatusContaPagar status) {
 		this.status = status;
 	}
 
@@ -143,6 +148,14 @@ public class ContaReceber implements Serializable {
 		this.pessoa = pessoa;
 	}
 
+	public Pessoa getPessoaFornecedor() {
+		return pessoaFornecedor;
+	}
+
+	public void setPessoaFornecedor(Pessoa pessoaFornecedor) {
+		this.pessoaFornecedor = pessoaFornecedor;
+	}
+
 	public FormaPagamento getFormaPagamento() {
 		return formaPagamento;
 	}
@@ -150,7 +163,6 @@ public class ContaReceber implements Serializable {
 	public void setFormaPagamento(FormaPagamento formaPagamento) {
 		this.formaPagamento = formaPagamento;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -165,7 +177,7 @@ public class ContaReceber implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		ContaReceber other = (ContaReceber) obj;
+		ContaPagar other = (ContaPagar) obj;
 		return Objects.equals(id, other.id);
 	}
 
---
+++ lojavirtual-backend/src/main/java/com/baggio/lojavirtualbackend/model/enums/StatusContaPagar.java
@@ -0,0 +1,28 @@
+package com.baggio.lojavirtualbackend.model.enums;
+
+public enum StatusContaPagar {
+
+	
+	COBRANCA("Pagar"),
+	VENCIDA("Vencida"),
+	ABERTA("Aberta"),
+	QUITADA("Quitada"),
+	ALUGUEL("Aluguel"),
+	FUNCIONARIO("Funcionario"),
+	RENEGOCIADA("Renegociada");
+	
+	private String descricao;
+	
+	private StatusContaPagar(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+}
