12af4bbddb42de065c53fc273465838ac1010ee6
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-04-18 14:30:22 -0300

    Criando Endpoint Acesso

--- lojavirtual-backend/src/main/java/com/baggio/lojavirtualbackend/controller/AcessoController.java
+++ lojavirtual-backend/src/main/java/com/baggio/lojavirtualbackend/controller/AcessoController.java
@@ -4,32 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baggio.lojavirtualbackend.model.Acesso;
 import com.baggio.lojavirtualbackend.service.AcessoService;
 
 @RestController
-@RequestMapping(value = "/acessos")
 public class AcessoController {
 	
 	@Autowired
 	private AcessoService acessoService;
 	
-	@PostMapping(value = "/salvarAcesso")
+
+	@ResponseBody
+	@PostMapping(value = "**/salvarAcesso")
 	public ResponseEntity<Acesso> salvar(@RequestBody Acesso acesso) {
 		Acesso acessoSalvo = acessoService.save(acesso); 		
-		return new ResponseEntity<>(acessoSalvo, HttpStatus.OK);
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
 	}
 	
-	@DeleteMapping(value = "/excluirAcesso/{id}")
-	public ResponseEntity<Acesso> excluir(@PathVariable Long id) {
-		acessoService.delete(id); 		
-		return new ResponseEntity<>(HttpStatus.OK);
+	@ResponseBody
+	@DeleteMapping(value = "**/excluirAcesso")
+	public ResponseEntity<Void> excluir(@RequestBody Acesso acesso) {
+		acessoService.delete(acesso.getId()); 		
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 
 }
--- lojavirtual-backend/src/main/java/com/baggio/lojavirtualbackend/security/WebConfigSecurity.java
+++ lojavirtual-backend/src/main/java/com/baggio/lojavirtualbackend/security/WebConfigSecurity.java
@@ -1,15 +1,24 @@
 package com.baggio.lojavirtualbackend.security;
 
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class WebConfigSecurity /* extends WebSecurityConfigurerAdapter implements HttpSessionListener */{
+import javax.servlet.http.HttpSessionListener;
 
-	/*@Override
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
+
+	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers(HttpMethod.GET, "/acessos/salvarAcesso")
-			.antMatchers(HttpMethod.POST, "/acessos/salvarAcesso")
-			.antMatchers(HttpMethod.DELETE, "/acessos/excluirAcesso/**");
-	}*/
+		web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso")
+			.antMatchers(HttpMethod.POST, "/salvarAcesso")
+			.antMatchers(HttpMethod.DELETE, "/excluirAcesso/**");
+	}
 	
 }
